db1816433311b773b727b27e571938d689a7a9e2
MOIS3Y <beyondbaikal@gmail.com>
2020-01-24 00:57:02 +0900

    Add documentation for api routes methods

--- app_template/api/api.py
+++ app_template/api/api.py
@@ -7,7 +7,13 @@ from ..api import bp
 
 
 def make_public_task(task):
-    """ change id to uri """
+    """
+    This is a helper function.
+    It changes the task id to the url task path.
+    Deletes the user id field.
+    This hides the IPA internal device from the user and makes the information
+    output more beautiful.
+    """
 
     url_task = {}
     for field in task:
@@ -18,19 +24,39 @@ def make_public_task(task):
             continue
         else:
             url_task[field] = task[field]
+
     return url_task
 
 
-# * curl -i http://localhost:5000/api/v.1.0/todo/tasks
+@bp.route('/v.1.0/ping')
+def ping():
+    """ Test route """
+    return jsonify({'response': 'Hello, friend'})
+
+
 @bp.route('/v.1.0/todo/tasks', methods=['GET'])
 @auth_required
 def get_tasks():
+    """
+    Generates a list of user tasks.
+    To access, you need to go through verification and get a token.
+    With each request it is necessary to transfer a token in the request
+    header. Based on this, the current user is determined and a database
+    query is made.
+
+    A simple request example:
+
+    curl -i -X GET
+    -H "Content-Type: application/json"
+    -H "Authorization: Bearer <your token>"
+    http://localhost:5000/api/v.1.0/todo/tasks
+
+    """
     user = current_user()
     response = []
-    # tasks = Tasks.query.all()
     tasks = Tasks.query.filter_by(user_id=user.id).all()
     if tasks:
-        # class serialization in JSON
+        # ? class serialization in JSON
         tasks = TasksSchema().dump(tasks, many=True)
         for task in tasks:
             response.append(make_public_task(task))
@@ -40,27 +66,58 @@ def get_tasks():
     return jsonify({'tasks': response})
 
 
-# * curl -i http://localhost:5000/api/v.1.0/todo/tasks/1 or 2,3....
 @bp.route('/v.1.0/todo/tasks/<int:task_id>', methods=['GET'])
 @auth_required
 def get_task(task_id):
+    """
+    Gets the user task.
+    To access, you need to go through verification and get a token.
+    With each request it is necessary to transfer a token in the request
+    header. Based on this, the current user is determined and a database
+    query is made.
+
+    A simple request example:
+
+    curl
+    -i -X GET
+    -H "Content-Type: application/json"
+    -H "Authorization: Bearer <your token>"
+    http://localhost:5000/api/v.1.0/todo/tasks/1 or 2,3....
+
+    """
     user = current_user()
-    # task = Tasks.query.get(task_id)
     task = Tasks.query.filter_by(user_id=user.id, id=task_id).first()
     if task:
-        # class serialization in JSON
+        # ? class serialization in JSON
         task = TasksSchema().dump(task)
         response = make_public_task(task)
-
         return jsonify({'task': response})
+
     abort(404)
 
 
-# * curl -i -H "Content-Type: application/json" -X POST
-# * -d '{"title":"Read a book"}' http://localhost:5000/api/v.1.0/todo/tasks
-@bp.route('/v.1.0/todo/tasks', methods=['POST'])
+@bp.route('/v.1.0/todo/tasks/new', methods=['POST'])
 @auth_required
 def create_task():
+    """
+    Creates a new task.
+    To access, you need to go through verification and get a token.
+    With each request it is necessary to transfer a token in the request
+    header. Based on this, the current user is determined, a task is created,
+    and a database query is made.
+    Mandatory field is the "title". Default 'done'=False
+
+    A simple request example:
+
+    curl
+    -i -X POST
+    -H "Content-Type: application/json"
+    -H "Authorization: Bearer <your token>"
+    -d '{"title":"Read a book"}'
+    http://localhost:5000/api/v.1.0/todo/tasks/new
+
+    """
+
     user = current_user()
     if not request.json or 'title' not in request.json:
         abort(400)
@@ -68,27 +125,45 @@ def create_task():
         title=request.json['title'],
         description=request.json.get('description', ""),
         user_id=user.id)
-
+    # *Add to db new task
     db.session.add(new_task)
     db.session.commit()
-
-    # last_task = Tasks.query.order_by(Tasks.id.desc()).first()
+    # *Get from db new task
     last_task = Tasks.query.filter_by(user_id=user.id).order_by(
         Tasks.id.desc()).first()
-
+    # *Show new task
     if last_task:
-        # class serialization in JSON
+        # ? class serialization in JSON
         task = TasksSchema().dump(last_task)
         response = make_public_task(task)
         return jsonify({'new_task': response}), 201
 
 
-# * curl -i -H "Content-Type: application/json" -X PUT
-# * -d '{"done":true}' http://localhost:5000/api/v.1.0/todo/tasks/2
 @bp.route('/v.1.0/todo/tasks/<int:task_id>', methods=['PUT'])
 @auth_required
 def update_task(task_id):
+    """
+    Updates a task.
+    To access, you need to go through verification and get a token.
+    With each request it is necessary to transfer a token in the request
+    header. Based on this, the current user is determined and a database
+    query is made.
+    Request verification - checks the request fields.Ignores extra fields
+    that are not defined in the "Tasks" class.
+
+    A simple request example:
+
+    curl
+    -i -X PUT
+    -H "Content-Type: application/json"
+    -H "Authorization: Bearer <your token>"
+    -d '{"done":true}'
+    http://localhost:5000/api/v.1.0/todo/tasks/2
+
+    """
+
     user = current_user()
+    # ? Request verification
     if not request.json:
         abort(400)
     if 'title' in request.json and type(request.json['title']) != str:
@@ -98,8 +173,9 @@ def update_task(task_id):
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
-    # task = Tasks.query.get(task_id)
+
     task = Tasks.query.filter_by(user_id=user.id, id=task_id).first()
+    # * Change task
     if task:
         task.title = request.json.get('title', task.title)
         task.description = request.json.get('description', task.description)
@@ -107,37 +183,43 @@ def update_task(task_id):
 
         db.session.add(task)
         db.session.commit()
-        # class serialization in JSON
+        # ? class serialization in JSON
         task = TasksSchema().dump(task)
         response = make_public_task(task)
         return jsonify({'task_update': response})
+
     abort(404)
 
 
-# * curl -i -H "Content-Type: application/json" -X DELETE
-# * http://localhost:5000/api/v.1.0/todo/tasks/2
 @bp.route('/v.1.0/todo/tasks/<int:task_id>', methods=['DELETE'])
 @auth_required
 def delete_task(task_id):
+    """
+    Deletes a task.
+    To access, you need to go through verification and get a token.
+    With each request it is necessary to transfer a token in the request
+    header. Based on this, the current user is determined and a database
+    query is made.
+
+    A simple request example:
+
+    curl
+    -i -X DELETE
+    -H "Content-Type: application/json"
+    -H "Authorization: Bearer <your token>"
+    http://localhost:5000/api/v.1.0/todo/tasks/1 or 2,3....
+
+    """
+
     user = current_user()
-    # task = Tasks.query.get(task_id)
     task = Tasks.query.filter_by(user_id=user.id, id=task_id).first()
     if task:
         db.session.delete(task)
         db.session.commit()
         response = {'task_delete': 'Success'}
         return jsonify(response)
+
     abort(404)
-
-
-@bp.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@bp.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({'error': 'Bad request'}), 400)
 
 
 # ! GUARD API
@@ -159,6 +241,14 @@ def login():
     return jsonify({'access_token': token})
 
 
+# * curl -i -H "Content-Type: application/json" -X POST
+# * -d '{"token":"<your token>"}' http://localhost:5000/api/refresh
+@bp.route('/refresh', methods=['POST'])
+def refresh():
+    token = guard.refresh_jwt_token(request.json['token'])
+    return jsonify({'update_token': token})
+
+
 # * curl -i -X GET -H "Authorization: Bearer <your token>"
 # * http://localhost:5000/api/protected
 @bp.route('/protected')
@@ -172,15 +262,13 @@ def protected():
         })
 
 
-# * curl -i -H "Content-Type: application/json" -X POST
-# * -d '{"token":"<your token>"}' http://localhost:5000/api/refresh
-@bp.route('/refresh', methods=['POST'])
-def refresh():
-    token = guard.refresh_jwt_token(request.json['token'])
-    return jsonify({'update_token': token})
+# ! Customization of standard error output
+
+@bp.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-# * Test route
-@bp.route('/v.1.0/ping')
-def ping():
-    return jsonify({'response': 'Hello, friend'})
+@bp.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
